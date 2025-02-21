03d31c72dbc07057f9b152ddc811f6e0f639a136
Josh Hayden <josh.hayden@tangoanalytics.com>
2025-02-21 15:16:15 -0600

    Add logging to the output directory

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -62,7 +62,9 @@ export const config: WebdriverIO.Config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'warn',
+    logLevel: 'debug',
+
+    outputDir: './output',
     //
     // Set specific log levels per logger
     // loggers:
